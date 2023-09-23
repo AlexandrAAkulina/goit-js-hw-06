f30c2bba03a5b22b0534e6e58f364c6de842457f
AlexandrAAkulina <icomfy1484@gmail.com>
2023-09-23 13:14:23 +0300

    add task 10

--- task-10/task-10.js
+++ task-10/task-10.js
@@ -1,5 +1,41 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+function main() {
+  const input = document.querySelector('input');
+  const createBtn = document.querySelector('[data-create]');
+  const destroyBtn = document.querySelector('[data-destroy]');
+  const boxes = document.querySelector('#boxes');
+
+
+  createBtn.addEventListener('click', () => {
+    createBoxes(input.value);
+  });
+  destroyBtn.addEventListener('click', destroyBoxes);
+  // console.log(input);
+
+  function createBoxes(amount) {
+    // const amount = input.value;
+
+    const baseSize = 30;
+
+    for (let i = 0; i < amount; i += 1) {
+      const box = document.createElement('div');
+      box.style.backgroundColor = getRandomHexColor();
+      const size = baseSize + i * 10;
+      box.style.width = `${size}px`;
+      box.style.height = `${size}px`;
+      boxes.appendChild(box);
+    }
+
+  }
+
+  function destroyBoxes() {
+    boxes.innerHTML = '';
+  }
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
 }
+
+main();
